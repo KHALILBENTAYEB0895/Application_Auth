dda1948fa522d0d8cc47b16719b7a70f45596473
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-06-23 19:25:22 +0100

    Backend : create register() function

---
+++ auth_backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        $token = $user->createToken($validated['name']);
+
+        return
+        [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+}
