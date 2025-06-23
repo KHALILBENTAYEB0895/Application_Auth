c0b15b939b146db17d1597716e1d3f9f5454186b
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-06-23 19:31:42 +0100

    Backend: create login() function

--- auth_backend/app/Http/Controllers/Auth/AuthController.php
+++ auth_backend/app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -20,6 +22,27 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken($validated['name']);
+
+        return
+        [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken($validated['email']);
 
         return
         [
